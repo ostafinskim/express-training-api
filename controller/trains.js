559d92a95bf2bc4c2af490959ab1cd4e87eaedfd
Miro Ostafiński <ostafinskim@gmail.com>
2023-07-04 16:55:30 +0100

    PUT

--- controller/trains.js
+++ controller/trains.js
@@ -56,7 +56,58 @@ const addNewTrain = async (req, res) => {
     }
 }
 
+const updateTrain = async (req, res) => {
+    const { id } = req.params;
+    const trainPayload = {
+        trainExpressName,
+        countryOfOrigin,
+        yearOfConstruction,
+        maxKilometerPerHour,
+        destinationFrom,
+        destinationTo,
+     } = req.body;
+    const token = String(id);
+    try {
+        readFile('public/data/trains.json', (err, data) => {
+            if (err) throw err;
+            const result = JSON.parse(data);
+            const item = result.filter(el => el.id === token);
+            if (!item) res.status(400).json({ msg: 'Item not found check ID'});
+            function updateElementById(array, id, updatedData) {
+                const updatedArray = array.map((element) => {
+                  if (element.id === id) {
+                    return { ...element, ...updatedData };
+                  }
+                  return element;
+                });
+              
+                return updatedArray;
+              }
+              function updateObjectExcludingId(oldObject, updatedProperties) {
+                const { id, ...rest } = updatedProperties;
+                return { ...oldObject, ...rest };
+              }
+              const updatedTrain = updateObjectExcludingId(item[0], trainPayload);              
+            
+            const updatedResults = updateElementById(result, token, updatedTrain).sort((a,b) => a.id - b.id);
+
+            writeFile('public/data/trains.json', JSON.stringify(updatedResults, null, 4), (err) => {
+                if (err) throw err;
+                res.status(200).json({
+                    msg: 'File has been updated'
+                })
+              })
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            msg: 'Service is not available at this time. Try again'
+        })
+    }
+}
+
 module.exports = {
     getAlltrains,
-    addNewTrain
+    addNewTrain,
+    updateTrain
 }
--- router/trains.js
+++ router/trains.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAlltrains, addNewTrain } = require('../controller/trains');
+const { getAlltrains, addNewTrain, updateTrain } = require('../controller/trains');
 
 const trainRouter = express.Router()
 
 trainRouter.route('/').get(getAlltrains);
 trainRouter.route('/').post(addNewTrain);
+
+trainRouter.route('/:id').put(updateTrain);
 
 module.exports = trainRouter;
