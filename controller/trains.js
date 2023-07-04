1dc773ddb1827c9759d74f20c072770167833108
Miro Ostafiński <ostafinskim@gmail.com>
2023-07-04 15:57:55 +0100

    POST

--- controller/trains.js
+++ controller/trains.js
@@ -1,5 +1,6 @@
 // mod.cjs
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { readFile, writeFile } = require('fs');
 
 const getAlltrains = async (req, res) => {
     try {
@@ -16,6 +17,46 @@ const getAlltrains = async (req, res) => {
     }
 }
 
+const addNewTrain = async (req, res) => {
+    const createTrainPayload = {
+        trainExpressName,
+        countryOfOrigin,
+        yearOfConstruction,
+        maxKilometerPerHour,
+        destinationFrom,
+        destinationTo,
+     } = req.body;
+
+     if (!trainExpressName || !countryOfOrigin || !yearOfConstruction || !maxKilometerPerHour || !destinationFrom || !destinationTo) {
+        res.status(400).json({
+            msg: 'Please provide valid data to enter new train'
+        })
+     }
+    try {
+        readFile('public/data/trains.json', (err, data) => {
+            if (err) throw err;
+            const result = JSON.parse(data);
+            const key = 'id';
+            const val = `${result.length + 1}`;
+            const newTrain = { [key]: val, ...createTrainPayload };
+            const updated = [...result, newTrain];
+
+        writeFile('public/data/trains.json', JSON.stringify(updated, null, 4), (err) => {
+            if (err) throw err;
+            res.status(200).json({
+                msg: 'File has been updated'
+            })
+          })
+        }); 
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            msg: 'Service is not available at this time. Try again'
+        })
+    }
+}
+
 module.exports = {
     getAlltrains,
+    addNewTrain
 }
