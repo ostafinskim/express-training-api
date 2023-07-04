2794db6f5c0238120d4f24281fe7c36b07351064
Miro Ostafiński <ostafinskim@gmail.com>
2023-07-04 17:04:04 +0100

    DELETE

--- router/trains.js
+++ router/trains.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAlltrains, addNewTrain, updateTrain } = require('../controller/trains');
+const { getAlltrains, addNewTrain, updateTrain, deleteTrain } = require('../controller/trains');
 
 const trainRouter = express.Router()
 
@@ -7,5 +7,6 @@ trainRouter.route('/').get(getAlltrains);
 trainRouter.route('/').post(addNewTrain);
 
 trainRouter.route('/:id').put(updateTrain);
+trainRouter.route('/:id').delete(deleteTrain);
 
 module.exports = trainRouter;
